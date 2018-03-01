129262716464921f84c1eaf547fce4df86294804
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-03-01 13:06:38 +0000

    let touch in and out record the entry and exit stations

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,38 +1,54 @@
 class Oystercard
+
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :exit_station, :journey, :history
 
   def initialize(balance: 0)
     @balance = balance
-    @in_journey = false
     @entry_station = entry_station
+    @exit_station = exit_station
+    @journey = {}
+    @history = []
   end
 
   def topup(amount)
-    fail 'You can not top up as it exceeds the limit' if @balance + amount > MAXIMUM_BALANCE
+    fail "You can't top up as it exceeds the limit" if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
 
   def in_journey?
-    @in_journey
+  	!@entry_station.nil?  	
   end
 
   def touch_in(entry_station)
-    fail 'Minimum balance required to touch in is £1' if @balance < MINIMUM_BALANCE
-    @in_journey = true
+  	fail "Minimum balance required to touch in is £1" if @balance < MINIMUM_BALANCE
     @entry_station = entry_station
+    @journey[:entry_station] = @entry_station
   end
+  
+  def touch_out(exit_station)
+  	deduct(1)
+  	@exit_station = exit_station
+    @journey[:exit_station] = @exit_station
+    @entry_station = nil   
+    @history << @journey
 
-  def touch_out
-    deduct(1)
-    @in_journey = false
-  end
+  end  	
+
+  def journey_history
+    history = []
+  end  	
 
   private
-
   def deduct(fare)
     @balance -= fare
   end
 end
+
+
+#teneray operator @in_journey == true ? true : false
+
+# journey = {entry_station: @entry_station, exit_station: @exit_station}
+# journey[:entry_station] = 
--- spec/oystercard_class_spec.rb
+++ spec/oystercard_class_spec.rb
@@ -1,68 +1,91 @@
-require 'oystercard.rb'
-
+require "oystercard.rb"
 describe Oystercard do
-  describe '#balace' do
-    it 'shows the amount of money that is left on card' do
+  let (:fake_station){double :fake_station}
+  describe "#balace" do
+    it "shows the amount of money that's left on card" do
       balance = 0
       expect(subject.balance).to eq balance
     end
   end
 
-  describe '#topup' do
-    it 'allows user to top up' do
-      expect { subject.topup 1 }.to change { subject.balance }.by 1
+  describe "#topup" do
+    it "allows user to top up" do
+      expect{ subject.topup 1 }.to change{ subject.balance }.by 1
     end
 
-    it 'raises an error when the amount exceeds the limit' do
+    it "raises an error when the amount exceeds the limit" do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
       subject.topup(maximum_balance)
-      message = 'You can not top up as it exceeds the limit'
-      expect { subject.topup 1 }.to raise_error message
+      message = "You can't top up as it exceeds the limit"
+      expect{ subject.topup 1 }.to raise_error message
     end
   end
 
-  describe '#deduct' do
+  describe '#deduct'do
     it 'deduces the fare for journey' do
       subject.topup(1)
-      fake_station = double('Liverpool Street')
       subject.touch_in(fake_station)
-      expect { subject.touch_out }.to change { subject.balance }.by(-1)
+      expect{ subject.touch_out(fake_station) }.to change{ subject.balance }.by -1
     end
   end
 
-  describe '#in_journey?' do
-    it 'returns true if in journey' do
+  describe "#in_journey?" do
+    it "returns true if in journey" do
       subject.topup(1)
-      fake_station = double('Liverpool Street')
       subject.touch_in(fake_station)
       expect(subject.in_journey?).to eq true
     end
+    it "returns false if not in journey" do
+      subject.entry_station == nil
+      expect(subject.in_journey?).to eq false
+    end  
   end
-
-  describe '#touch_in(fake_station)' do
-    it 'raises an error when the balance is below £1' do
-      # minimum_balance = Oystercard::MINIMUM_BALANCE
-      message = 'Minimum balance required to touch in is £1'
-      fake_station = double('Liverpool Street')
+  
+  describe "#touch_in(fake_station)" do
+    it "raises an error when the balance is below £1" do
+      message = "Minimum balance required to touch in is £1"
       expect { subject.touch_in(fake_station) }.to raise_error message
-    end
-
-    it 'remembers the station you touched the card in' do
+    end  
+    it "remembers the station you touched the card in" do
       subject.topup(1)
-      fake_station = double('Liverpool Street')
       subject.touch_in(fake_station)
       expect(subject.entry_station).to eq fake_station
-    end
-  end
-
-  describe '#touch_out' do
-    it 'returns true if you touch out' do
-      expect(subject.touch_out).to eq false
-    end
-
-    it 'deducts a fare for the journey' do
+    end 
+    it "updates a hash with an entry station" do
       subject.topup(1)
-      expect { subject.touch_out }.to change { subject.balance }.by(-1)
-    end
+      subject.touch_in(fake_station)
+      expect( subject.journey[:entry_station]).to eq fake_station 
+    end  
+
   end
+
+  describe "#touch_out(exit_station)" do
+    before (:each) do
+      subject.topup(1)
+    end  
+    it "deducts a fare for the journey" do
+      expect { subject.touch_out(fake_station) }.to change { subject.balance }.by -1 
+    end  
+    it "forgets the station it touched in" do
+      subject.touch_in(fake_station)
+      subject.touch_out(fake_station)
+      expect( subject.entry_station).to eq nil
+    end
+    it "updates a hash with an exit station" do 
+      subject.touch_in(fake_station)
+      subject.touch_out(fake_station)
+      expect( subject.journey[:exit_station]).to eq fake_station
+    end
+    # it "makes a hash of entry station and exit station" do
+    #   expect(subject.touch_out).to be_an_instance_of(Hash)
+    # end    
+  end   
+
+  describe '#journey_history' do
+    it "shows a history of journey including entry station and exit station" do
+      expect(subject.journey_history).to be_an_instance_of(Array)
+    end
+  end              
 end
+
+
